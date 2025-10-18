5e29d04451a07a4666d183bb58b2b46096bdd8e3
TrindadeDaniel <daniel.henrique230207@gmail.com>
2025-10-17 21:29:19 -0300

    Add files via upload
    dom / javascript

---
+++ aula7/DOM/js/javascript.js
@@ -0,0 +1,10 @@
+function exibir(){
+   let paragrafo = document.getElementById("nome");
+   console.log(paragrafo);
+   paragrafo.innerHTML ="FORA LULA!!"
+}
+function ocultar(){
+    let paragrafo = document.getElementById("nome");
+   console.log(paragrafo);
+   paragrafo.innerHTML =""
+}
